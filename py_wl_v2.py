7737ba51a2e8e3ab36a79678ae0b77709d7e23c8
brandonbeasley <tbrandonbeasley@gmail.com>
2019-11-08 15:26:41 -0500

    various fixes around signature

--- py_wl_v2.py
+++ py_wl_v2.py
@@ -12,6 +12,7 @@ import hmac
 import hashlib
 import requests
 import collections
+from pprint import pprint
 
 
 class WeatherLink:
@@ -23,18 +24,13 @@ class WeatherLink:
         self.api_secret = api_secret.encode('utf-8')
         self.timestamp = int(time.time())
 
-    def signature(self, station_id=''):
+    def signature(self, params={}):
         self.timestamp = int(time.time())
 
-        params = {
-            'api-key': self.api_key,
-            't': self.timestamp
-        }
+        params['api-key'] = self.api_key
+        params['t'] = self.timestamp
 
-        if station_id != '':
-            params['station-id'] = station_id
-
-        # sort keys in alpha order
+        # sort params in alpha order
         params = collections.OrderedDict(sorted(params.items()))
 
         payload = ''
@@ -48,10 +44,11 @@ class WeatherLink:
     def stations(self, station_ids=[]):
         # accepts a list of station ids in string format
         if station_ids:
-            stations_str = ''
-            stations_str.join(station_ids)
+            stations_str = ','.join(station_ids)
+            pprint(stations_str)
             req = requests.get(self.API_URL_V2 + 'stations/' + stations_str + '?api-key=' + self.api_key +
-                               '&api-signature=' + self.signature() + '&t=' + str(self.timestamp))
+                               '&api-signature=' + self.signature({'station-ids': stations_str}) + '&t=' +
+                               str(self.timestamp))
         else:
             req = requests.get(self.API_URL_V2 + 'stations?api-key=' + self.api_key + '&api-signature=' +
                                self.signature() + '&t=' + str(self.timestamp))
@@ -60,10 +57,9 @@ class WeatherLink:
     def nodes(self, node_ids=[]):
         # accepts a list of node ids in string format
         if node_ids:
-            nodes_str = ''
-            nodes_str.join(node_ids)
+            nodes_str = ','.join(node_ids)
             req = requests.get(self.API_URL_V2 + 'nodes/' + nodes_str + '?api-key=' + self.api_key + '&api-signature=' +
-                               self.signature() + '&t=' + str(self.timestamp))
+                               self.signature({'node-ids': nodes_str}) + '&t=' + str(self.timestamp))
         else:
             req = requests.get(self.API_URL_V2 + 'nodes?api-key=' + self.api_key + '&api-signature=' +
                                self.signature() + '&t=' + str(self.timestamp))
@@ -72,10 +68,11 @@ class WeatherLink:
     def sensors(self, sensor_ids=[]):
         # accepts a list of sensor ids in string format
         if sensor_ids:
-            sensors_str = ''
-            sensors_str.join(sensor_ids)
+            sensors_str = ','.join(sensor_ids)
+
             req = requests.get(self.API_URL_V2 + 'sensors/' + sensors_str + '?api-key=' + self.api_key +
-                               '&api-signature=' + self.signature() + '&t=' + str(self.timestamp))
+                               '&api-signature=' + self.signature({'sensor-ids': sensors_str}) + '&t=' +
+                               str(self.timestamp))
         else:
             req = requests.get(self.API_URL_V2 + 'sensors?api-key=' + self.api_key + '&api-signature=' +
                                self.signature() + '&t=' + str(self.timestamp))
@@ -84,10 +81,9 @@ class WeatherLink:
     def sensor_activity(self, sensor_ids=[]):
         # accepts a list of sensor ids in string format
         if sensor_ids:
-            sensors_str = ''
-            sensors_str.join(sensor_ids)
+            sensors_str = ','.join(sensor_ids)
             req = requests.get(self.API_URL_V2 + 'sensor-activity/' + sensors_str + '?api-key=' + self.api_key +
-                               '&api-signature=' + self.signature() + '&t=' + str(self.timestamp))
+                               '&api-signature=' + self.signature({'sensor-ids': sensors_str}) + '&t=' + str(self.timestamp))
         else:
             req = requests.get(self.API_URL_V2 + 'sensor-activity?api-key=' + self.api_key + '&api-signature=' +
                                self.signature() + '&t=' + str(self.timestamp))
@@ -99,46 +95,66 @@ class WeatherLink:
 
     def current(self, station_id):
         return requests.get(self.API_URL_V2 + 'current/' + str(station_id) + '?api-key=' + self.api_key +
-                            '&api-signature=' + self.signature(station_id) + '&t=' + str(self.timestamp))
+                            '&api-signature=' + self.signature({'station-id': station_id}) + '&t=' + str(self.timestamp))
 
     def historic(self, station_id, start_timestamp, end_timestamp):
-        return requests.get(self.API_URL_V2 + 'current/' + str(station_id) + '?api-key=' + self.api_key +
-                            '&api-signature=' + self.signature(station_id) + '&t=' + str(self.timestamp) +
-                            '&start-timestamp=' + str(start_timestamp) + '&end-timestamp=' + str(end_timestamp))
+
+        pprint(self.API_URL_V2 + 'historic/' + str(station_id) + '?api-key=' + self.api_key +
+                            '&api-signature=' + self.signature({'station-id': station_id, 'start-timestamp': start_timestamp, 'end_timestamp': end_timestamp}) +
+                            '&t=' + str(self.timestamp) + '&start-timestamp=' + str(start_timestamp) +
+                            '&end-timestamp=' + str(end_timestamp))
+
+        return requests.get(self.API_URL_V2 + 'historic/' + str(station_id) + '?api-key=' + self.api_key +
+                            '&api-signature=' + self.signature({'station-id': station_id, 'start-timestamp': start_timestamp, 'end-timestamp': end_timestamp}) +
+                            '&t=' + str(self.timestamp) + '&start-timestamp=' + str(start_timestamp) +
+                            '&end-timestamp=' + str(end_timestamp))
+
+# Responses are returned as Request objects, ( .body .text .json() )
 
 
 # Create a WeatherLink object using API keys
-wl = WeatherLink('your_api_key', 'your_api_secret')
+wl = WeatherLink('api_key', 'api_secret')
 
 # Get Stations
-wl.stations()
+pprint(wl.stations().json())
+
 
 # Get Specific Stations (Accepts a list of Station Ids in string format)
-wl.stations(['12345', '45678'])
+pprint(wl.stations(['12345', ]).json())
+
 
 # Get Nodes
-wl.nodes()
+pprint(wl.nodes().json())
 
 # Get Specific Nodes (Accepts a list of Node Ids in string format)
 wl.nodes(['12345', '45678'])
 
+
 # Get Sensors
-wl.sensors()
+pprint(wl.sensors().json())
+
 
 # Get Specific Sensors (Accepts a list of Sensor Ids in string format)
-wl.sensors(['12345', '45678'])
+pprint(wl.sensors(['222222', '333333']).json())
+
 
 # Get Sensor activity
-wl.sensor_activity()
+pprint(wl.sensor_activity().json())
+
 
 # Get Specific Sensor activity (Accepts a list of Sensor Ids in string format)
-wl.sensor_activity(['12345', '45678'])
+pprint(wl.sensor_activity(['222222', '333333']).json())
+
 
 # Get Sensor catalog
-wl.sensor_catalog()
+# pprint(wl.sensor_catalog().json())
+
 
 # Get current conditions for a station
-wl.current('12345')
+pprint(wl.current('12345').json())
+
 
 # Get historic conditions for a station
-wl.historic('12345', '1573237000', '1573238061')
+pprint(wl.historic('12345', '1573237900', '1573238061').json())
+
+
